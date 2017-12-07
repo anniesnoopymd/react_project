8957f55c63c5bae88bda7af54d2f7a663c2f24cb
anniesnoopymd <anniesnoopymd@gmail.com>
2017-12-07 23:33:09 +0800

    webpack config

---
+++ src/webpack.config.js
@@ -0,0 +1,11 @@
+var webpack = require("webpack");
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname, "dist"); //where to copy it
+var SRC_DIR = path.resolve(__dirname, "src"); // where to see uncompiling code
+
+var config = { //holds the webpack configuration
+  entry =   //tell webpack which file you should start your transpiling and building your journey in
+}
+
+module.exports = config;
